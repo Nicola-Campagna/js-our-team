af5b7943bd90ac9bca1f407f41ca876f1ea7f5f4
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-06 16:40:33 +0100

    my team in console (array di oggetti)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,58 @@
-// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. OK
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto. OK
 // MILESTONE 0:
-// Creare l’array di oggetti con le informazioni fornite.
+// Creare l’array di oggetti con le informazioni fornite. OK
 // MILESTONE 1:
-// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto OK
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+// Array di object per rappresentare il team
+const arrayTeam = [
+    // object con le rispettive informazioni
+
+    // object 0
+    {
+        nome: "Wayne Barnett",
+        ruolo: "Founder e CEO",
+        foto: "wayne-barnett-founder-ceo.jpg",
+    },
+
+    // object 1
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg"
+    },
+
+    // object 2
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg"
+    },
+
+    // object 3
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg"
+    },
+
+    // object 4
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg"
+    },
+
+    // object 5
+    {
+        nome: "Graphic Designer",
+        ruolo: "barbara-ramos-graphic-designer.jpg",
+        foto: "Barbara Ramos"
+    }
+]
+
+console.log(arrayTeam);
 
