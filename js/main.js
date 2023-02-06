05c7ea754512f27668f95c5f1b0a2846292c0779
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-06 18:41:13 +0100

    info del team sul dom

--- js/main.js
+++ js/main.js
@@ -9,50 +9,106 @@
 
 // Array di object per rappresentare il team
 const arrayTeam = [
-    // object con le rispettive informazioni
 
-    // object 0
-    {
-        nome: "Wayne Barnett",
-        ruolo: "Founder e CEO",
-        foto: "wayne-barnett-founder-ceo.jpg",
-    },
+    persona = [
+        // object con le rispettive informazioni
 
-    // object 1
-    {
-        nome: "Angela Caroll",
-        ruolo: "Chief Editor",
-        foto: "angela-caroll-chief-editor.jpg"
-    },
+        // object 0
+        {
+            nome: "Wayne Barnett",
+            ruolo: "Founder e CEO",
+            foto: "wayne-barnett-founder-ceo.jpg",
+        },
 
-    // object 2
-    {
-        nome: "Walter Gordon",
-        ruolo: "Office Manager",
-        foto: "walter-gordon-office-manager.jpg"
-    },
 
-    // object 3
-    {
-        nome: "Angela Lopez",
-        ruolo: "Social Media Manager",
-        foto: "angela-lopez-social-media-manager.jpg"
-    },
+        // object 1
+        {
+            nome: "Angela Caroll",
+            ruolo: "Chief Editor",
+            foto: "angela-caroll-chief-editor.jpg"
+        },
 
-    // object 4
-    {
-        nome: "Scott Estrada",
-        ruolo: "Developer",
-        foto: "scott-estrada-developer.jpg"
-    },
+        // object 2
+        {
+            nome: "Walter Gordon",
+            ruolo: "Office Manager",
+            foto: "walter-gordon-office-manager.jpg"
+        },
 
-    // object 5
-    {
-        nome: "Graphic Designer",
-        ruolo: "barbara-ramos-graphic-designer.jpg",
-        foto: "Barbara Ramos"
-    }
+        // object 3
+        {
+            nome: "Angela Lopez",
+            ruolo: "Social Media Manager",
+            foto: "angela-lopez-social-media-manager.jpg"
+        },
+
+        // object 4
+        {
+            nome: "Scott Estrada",
+            ruolo: "Developer",
+            foto: "scott-estrada-developer.jpg"
+        },
+
+        // object 5
+        {
+            nome: "Barbara Ramos",
+            ruolo: "Graphic Dedigner",
+            foto: "barbara-ramos-graphic-designer.jpg"
+        }
+
+    ]
+
 ]
 
-console.log(arrayTeam);
+for (proprieta in persona) {
+    console.log(persona[proprieta]);
+}
+
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// creare un div e inserire l'oggetto con le rispettive info...x6
+const cardTeamEl = document.getElementById("container-team-cards");
+cardTeamEl.innerHTML = `
+<div>
+    ${persona[0].nome} <br>
+    ${persona[0].ruolo}<br>
+    ${persona[0].foto}
+</div>
+<br>
+
+<div>
+    ${persona[1].nome} <br>
+    ${persona[1].ruolo}<br>
+    ${persona[1].foto}
+</div>
+<br>
+
+<div>
+    ${persona[2].nome} <br>
+    ${persona[2].ruolo}<br>
+    ${persona[2].foto}
+</div>
+<br>
+
+<div>
+    ${persona[3].nome} <br>
+    ${persona[3].ruolo}<br>
+    ${persona[3].foto}
+</div>
+<br>
+
+<div>
+    ${persona[4].nome} <br>
+    ${persona[4].ruolo}<br>
+    ${persona[4].foto}
+</div>
+<br>
+
+<div>
+    ${persona[5].nome} <br>
+    ${persona[5].ruolo}<br>
+    ${persona[5].foto}
+</div>
+    `
+
+
 
